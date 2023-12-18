9c4a5cb2172933ce6ac3baef4597333a79f2ef2f
null <jun_12@163.com>
2023-12-18 15:24:22 +0800

    更新

--- src/main/java/com/example/demo/Demo7Application.java
+++ src/main/java/com/example/demo/Demo7Application.java
@@ -9,7 +9,6 @@ public class Demo7Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Demo7Application.class, args);
-        System.out.println("tttttttttttttttttt");
     }
 
 }
