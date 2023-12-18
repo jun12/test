562de6c78c74d36a6d1fee3f6223d2a279636ce0
null <jun_12@163.com>
2023-12-18 15:12:28 +0800

    更新

--- src/main/java/com/example/demo/Demo7Application.java
+++ src/main/java/com/example/demo/Demo7Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo7Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Demo7Application.class, args);
+        System.out.println("tttttttttttttttttt");
     }
 
 }
